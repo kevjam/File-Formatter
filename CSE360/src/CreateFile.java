ecd19962d12e1a59ae3d6f7c6c3c27688d05bc4e
Calvin <cdnorman@asu.edu>
2018-04-15 20:55:05 -0700

    Add files via upload

--- CSE360/src/CreateFile.java
+++ CSE360/src/CreateFile.java
@@ -21,7 +21,7 @@ public class CreateFile {
 			writeLeftJustified(line, spacing, newline);
 		} else {
 			// Connor: Calvin, his is where you will likely add your new function call for writeFullJustified
-			
+			writeFullJustified(line, newline, spacing, linelength);
 		} // End of if else statement
 	} // End of writeToFile method
 	
@@ -56,10 +56,47 @@ public class CreateFile {
 		} // End of outer if else statement
 	} // End of writeRightJustified method
 	
-	// Connor: Calvin, it's up to you how you want to do this. I suggest following the format of
-	// the other two justified methods here
-	public void writeFullJustified() {
-		
+	public void writeFullJustified(String line, boolean newline, String spacing, int linelength) {
+		//First we need to compare line's length to the linelength variable
+		int difference = linelength - line.length();
+		//Only modify line if difference is greater than 0. A negative value indicates that it's just one
+		//Big word, and a zero value indicates that we're fine
+		if (difference > 0) {
+			String[] splitLine = line.split(" ");
+			int spacesToAdd = difference;
+			while (spacesToAdd > 0)
+            {
+                for (int i = 0; i < splitLine.length - 1 && spacesToAdd > 0; i++)
+                {
+                    splitLine[i] = splitLine[i] + " ";
+                    spacesToAdd--;
+                }
+            }
+			//Now we recombine the line
+			String newLine = "";
+			for (int i = 0; i < splitLine.length; i++)
+			{
+				//First we have a case for the last item in the array
+				if (i == splitLine.length - 1)
+					newLine = newLine + splitLine[i];
+				else
+					newLine = newLine + splitLine[i] + " ";
+			}
+			line = newLine;
+		}
+		//If we want to add a new line
+		if(newline) {
+			//If we are using single space
+			if(spacing.equals("single")) {
+				formatter.format("%s%n", line);
+			//Else we're using double space
+			} else {
+				formatter.format("%s%n%n", line);
+			}//End of inner if else statement
+			//Else add the new line to the end of the current line
+		} else {
+			formatter.format("%s", line);
+		}	
 	} // End of writeFullJustified method
 	
 	// Closes the formatter object
@@ -68,3 +105,4 @@ public class CreateFile {
 	} // End of closeFile method
 	
 } // End of CreateFile class
+
